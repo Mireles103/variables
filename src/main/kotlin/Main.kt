b7f8dfa66501afa469de460cb12bcd6b9d79cb0b
Jose Mireles <Mirelesjose379@gmail.com>
2023-01-24 21:39:26 -0600

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val byte = 8
+    val myShort = 10
+    val myInt = 55
+    val myLong = 40L
+    val myFloat = 59.73F
+    val myDouble = 31.42
+    val myBoolean = true
+    val myChar = 'A'
+
+    println("Byte demo: $byte")
+    println("short demo: $myShort")
+    println("Int demo: $myInt")
+    println("Long demo: $myLong")
+    println("Float demo: $myFloat")
+    println("Double demo: $myDouble")
+    println("Boolean demo: $myBoolean")
+    println("Char demo: $myChar")
+}
